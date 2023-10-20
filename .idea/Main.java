78292ad2d638e6cf65c6c9c3b9e0e69aa47a1d34
Patryk <patryk.pach@onet.pl>
2023-10-20 19:04:08 +0200

    Testowanie przykładowego programy do generacji histogramu

--- .idea/Main.java
+++ .idea/Main.java
@@ -1,17 +1,75 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.print("Wczytać z plików (P) czy wprowadzić tekst ręcznie (R)? ");
+            String opcja = scanner.next().toLowerCase();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            if (opcja.equals("p")) {
+                System.out.print("Podaj ścieżkę do katalogu z plikami tekstowymi: ");
+                String katalog = scanner.next();
+                Map<Character, Integer> zliczenieLiter = przetworzKatalog(katalog);
+                wyswietlHistogram(zliczenieLiter);
+                break;
+            } else if (opcja.equals("r")) {
+                System.out.print("Wprowadź tekst: ");
+                String tekst = scanner.next();
+                Map<Character, Integer> zliczenieLiter = zliczLiteryWTekscie(tekst);
+                wyswietlHistogram(zliczenieLiter);
+                break;
+            } else {
+                System.out.println("Nieprawidłowa opcja. Wybierz 'P' lub 'R'.");
+            }
+        }while(true);
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static Map<Character, Integer> zliczLiteryWTekscie(String tekst) {
+        Map<Character, Integer> zliczenieLiter = new HashMap<>();
+        for (char znak : tekst.toCharArray()) {
+            if (Character.isLetter(znak)) {
+                znak = Character.toLowerCase(znak);
+                zliczenieLiter.put(znak, zliczenieLiter.getOrDefault(znak, 0) + 1);
+            }
+        }
+        return zliczenieLiter;
+    }
+
+    public static Map<Character, Integer> przetworzKatalog(String sciezkaKatalogu) {
+        Map<Character, Integer> zliczenieLiter = new HashMap<>();
+        File katalog = new File(sciezkaKatalogu);
+        if (!katalog.exists() || !katalog.isDirectory()) {
+            System.out.println("Podany katalog nie istnieje.");
+            return zliczenieLiter;
+        }
+
+        File[] pliki = katalog.listFiles();
+        if (pliki != null) {
+            for (File plik : pliki) {
+                if (plik.isFile() && plik.getName().endsWith(".txt")) {
+                    try (BufferedReader reader = new BufferedReader(new FileReader(plik))) {
+                        String linia;
+                        while ((linia = reader.readLine()) != null) {
+                            Map<Character, Integer> zliczenieLiterek = zliczLiteryWTekscie(linia);
+                            zliczenieLiterek.forEach((litera, ilosc) -> zliczenieLiter.merge(litera, ilosc, Integer::sum));
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return zliczenieLiter;
+    }
+
+    public static void wyswietlHistogram(Map<Character, Integer> zliczenieLiter) {
+        System.out.println("Histogram częstości liter:");
+        for (Map.Entry<Character, Integer> entry : zliczenieLiter.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
