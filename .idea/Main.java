32a9fa6456f7dbd8d86907f31432e1f2d53bfc28
Patryk <patryk.pach@onet.pl>
2023-10-20 20:08:49 +0200

    Zmiana na własne podejście, utworzenie systemu zliczającego znaki

--- .idea/Main.java
+++ .idea/Main.java
@@ -1,78 +1,54 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        do{
+        do {
             System.out.print("Wczytać z plików (P) czy wprowadzić tekst ręcznie (R)? ");
             String opcja = scanner.next().toLowerCase();
 
             if (opcja.equals("p")) {
                 System.out.print("Podaj ścieżkę do katalogu z plikami tekstowymi: ");
                 String katalog = scanner.next();
-                Map<Character, Integer> zliczenieLiter = przetworzKatalog(katalog);
-                if(zliczenieLiter != null){
-                    wyswietlHistogram(zliczenieLiter);
-                }
+//NAPISAĆ METODĘ SPRAWDZAJĄCA CZY KATALOG I PLIKI (*.txt) ISTNIEJĄ
+//JEŚLI TAK TO ZCZYTUJEMY Z NICH TEKST I WYSYŁAMY DO ????utworzListyCharInt(tekst)?????
                 break;
             } else if (opcja.equals("r")) {
                 System.out.print("Wprowadź tekst: ");
                 String tekst = scanner.next();
-                Map<Character, Integer> zliczenieLiter = zliczLiteryWTekscie(tekst);
-                wyswietlHistogram(zliczenieLiter);
+                Object wystepowanie = utworzListyCharInt(tekst);
                 break;
             } else {
                 System.out.println("Nieprawidłowa opcja. Wybierz 'P' lub 'R'.");
             }
-        }while(true);
+        } while (true);
     }
 
-    public static Map<Character, Integer> zliczLiteryWTekscie(String tekst) {
-        Map<Character, Integer> zliczenieLiter = new HashMap<>();
-        for (char znak : tekst.toCharArray()) {
-            if (Character.isLetter(znak)) {
-                znak = Character.toLowerCase(znak);
-                zliczenieLiter.put(znak, zliczenieLiter.getOrDefault(znak, 0) + 1);
-            }
-        }
-        return zliczenieLiter;
-    }
+    //Zliczanie liter (ASCII 97 - 122)
+    public static Object utworzListyCharInt(String tekst) {
+        List<Character> listaZnakow = new ArrayList<>();
+        List<Integer> listaWystepowania = new ArrayList<>();
 
-//SPRAWDZONE---
-    public static Map<Character, Integer> przetworzKatalog(String sciezkaKatalogu) {
-        Map<Character, Integer> zliczenieLiter = new HashMap<>();
-        File katalog = new File(sciezkaKatalogu);
-        if (!katalog.exists() || !katalog.isDirectory()) {
-            System.out.println("Podany katalog nie istnieje.");
-            return zliczenieLiter;
-        }
-
-        File[] pliki = katalog.listFiles();
-        if (pliki != null) {
-            for (File plik : pliki) {
-                if (plik.isFile() && plik.getName().endsWith(".txt")) {
-                    try (BufferedReader reader = new BufferedReader(new FileReader(plik))) {
-                        String linia;
-                        while ((linia = reader.readLine()) != null) {
-                            Map<Character, Integer> zliczenieLiterek = zliczLiteryWTekscie(linia);
-                            zliczenieLiterek.forEach((litera, ilosc) -> zliczenieLiter.merge(litera, ilosc, Integer::sum));
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        //Sprawdzanie powtarzalności i zapisywanie o jaki znak chodzi do listy listaZnakow oraz ile razy występuje w liście listaWystapien
+        for (int i = 97; i <= 122; i++) {
+            int counter = 0;
+            for (char znak: tekst.toCharArray()) {
+                if(znak == i){
+                    counter++;
                 }
             }
+            char znak = (char) i;
+            listaZnakow.add(znak);
+            listaWystepowania.add(counter);
         }
-        return zliczenieLiter;
-    }
 
-    public static void wyswietlHistogram(Map<Character, Integer> zliczenieLiter) {
-        System.out.println("Histogram częstości liter:");
-        for (Map.Entry<Character, Integer> entry : zliczenieLiter.entrySet()) {
-            System.out.println(entry.getKey() + " - " + entry.getValue());
-        }
+        List<Object> wynik = new ArrayList<>();
+        wynik.add(listaZnakow);
+        wynik.add(listaWystepowania);
+        return wynik;
     }
 }
+
