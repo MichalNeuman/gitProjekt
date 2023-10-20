dfc7ed7bbc618fd67f7c5d379f08a2c6babdbc6b
Patryk <patryk.pach@onet.pl>
2023-10-20 21:12:10 +0200

    Zmiana kodu

--- .idea/Main.java
+++ .idea/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String tekst = "ala ma kota";
+        String tekst = "Ala ma kota!";
         /*
         do {
             System.out.print("Wczytać z plików (P) czy wprowadzić tekst ręcznie (R)? ");
